ee6dc69f49fa85cc96e37c8baa761df3befbfb20
zwd <17326195691@163.com>
2023-10-09 16:18:15 +0800

    添加文件存储路径

--- src/main/index.js
+++ src/main/index.js
@@ -1,15 +1,14 @@
-import { app, shell, BrowserWindow } from 'electron'
-import { join } from 'path'
+import { app, shell, BrowserWindow, ipcMain, dialog } from 'electron'
+import { join } from 'node:path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
 
 function createWindow() {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 540,
     height: 500,
     show: false,
-    alwaysOnTop: true,
+    // alwaysOnTop: true,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
@@ -18,6 +17,11 @@ function createWindow() {
     },
     icon: icon
   })
+
+  ipcMain.on('dialog:openFile', (_, path) => {
+    shell.openPath(path)
+  })
+  ipcMain.handle('dialog:changeFile', handleFileOpen)
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
@@ -28,8 +32,6 @@ function createWindow() {
     return { action: 'deny' }
   })
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
@@ -37,16 +39,9 @@ function createWindow() {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
@@ -54,20 +49,25 @@ app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
+async function handleFileOpen() {
+  const { canceled, filePaths } = await dialog.showOpenDialog({
+    title: '选择待办日志文件夹',
+    properties: ['openDirectory']
+  })
+  if (canceled) {
+    console.log('canceled')
+    return
+  } else {
+    return filePaths[0]
+  }
+}
--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,22 +1,41 @@
 <script setup>
-import { Modal, Tag } from 'ant-design-vue'
+import { computed, ref } from 'vue'
+import { Modal, Tag, Tabs, TabPane, Button } from 'ant-design-vue'
 import { QuestionCircleOutlined } from '@ant-design/icons-vue'
 import dayjs from 'dayjs'
 import TodoList from './components/todo-list.vue'
 import ShortcutKey from './components/shortcut-key.vue'
 
-import { computed, onMounted, ref } from 'vue'
-const open = ref(false)
-
-const showModal = () => {
-  open.value = true
-}
 const today = computed(() => dayjs().format('YYYY-MM-DD'))
+
+let open = ref(false)
+let activeKey = ref('1')
+function showModal() {
+  open.value = true
+  activeKey.value = '1'
+}
+
+let fileUrl = ref('')
+function changeFileStorage() {
+  console.log('🔥🔥🔥🔥🔥🔥', window.electron)
+
+  const { ipcRenderer } = window.electron
+  ipcRenderer.invoke('dialog:changeFile').then((res) => {
+    console.log('file', res)
+    fileUrl.value = res
+  })
+}
+
+function openFile() {
+  console.log('🔥🔥🔥🔥🔥🔥', window.electronFile)
+  if (!fileUrl.value) return
+  window.electronFile.setFileUrl(fileUrl.value)
+}
 </script>
 
 <template>
   <div class="main">
-    <header>
+    <header @click="handle">
       <Tag :bordered="false">{{ today }}</Tag>
       待办事项
     </header>
@@ -25,12 +44,53 @@ const today = computed(() => dayjs().format('YYYY-MM-DD'))
     <div class="tipBox" title="快捷键提示" @click="showModal">
       <QuestionCircleOutlined style="font-size: 20px" />
     </div>
-    <Modal v-model:open="open" title="快捷键" :footer="null">
-      <ShortcutKey></ShortcutKey>
+    <Modal v-model:open="open" title="帮助" :footer="null">
+      <Tabs v-model:activeKey="activeKey">
+        <TabPane key="1" tab="快捷键">
+          <ShortcutKey></ShortcutKey>
+        </TabPane>
+        <TabPane key="2" tab="文件管理">
+          <div class="file-manage">
+            <div class="row-url">{{ fileUrl }}</div>
+            <div class="row-tip">待办日志文件的默认保存位置</div>
+            <div class="row-btns">
+              <Button @click="changeFileStorage">更改</Button>
+              <Button @click="openFile">打开文件夹</Button>
+            </div>
+          </div>
+        </TabPane>
+      </Tabs>
     </Modal>
   </div>
 </template>
 
 <style lang="less">
 @import './assets/css/app.less';
+
+.file-manage {
+  height: 192px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  user-select: none;
+  .row-url {
+    box-sizing: border-box;
+    min-width: 184px;
+    height: 32px;
+    border: 1px solid #ccc;
+    padding: 4px 10px 4px 2px;
+  }
+  .row-tip {
+    font-size: 12px;
+    color: #666;
+    padding-bottom: 24px;
+  }
+  .row-btns {
+    width: 184px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 12px;
+  }
+}
 </style>
